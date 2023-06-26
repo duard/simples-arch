47d6487f85a167bc42914e70f260ff1ce9e5757b
Carlos Developer <duard.developer@gmail.com>
2023-06-26 19:25:22 -0300

    feat: fill tables

--- config/sql/base_3.sql
+++ config/sql/base_3.sql
@@ -6,14 +6,31 @@ CREATE TABLE "persons" (
   "name" varchar(50) NOT NULL
 );
 
-CREATE TABLE "addresses" (
+
+CREATE TABLE "roles" (
   "id" int PRIMARY KEY,
+  "role_name" varchar(50) NOT NULL
+);
+
+CREATE TABLE "person_roles" (
   "person_id" int,
-  "address_line" varchar(100) NOT NULL,
-  "city" varchar(50) NOT NULL,
-  "state" varchar(50) NOT NULL,
-  "country" varchar(50) NOT NULL,
-  FOREIGN KEY ("person_id") REFERENCES "persons" ("id")
+  "role_id" int,
+  FOREIGN KEY ("person_id") REFERENCES "persons" ("id"),
+  FOREIGN KEY ("role_id") REFERENCES "roles" ("id"),
+  PRIMARY KEY ("person_id", "role_id")
+);
+
+CREATE TABLE "address_types" (
+  "id" int PRIMARY KEY,
+  "type" varchar(50) NOT NULL
+);
+
+CREATE TABLE "address_type_association" (
+  "address_id" int,
+  "type_id" int,
+  FOREIGN KEY ("address_id") REFERENCES "addresses" ("id"),
+  FOREIGN KEY ("type_id") REFERENCES "address_types" ("id"),
+  PRIMARY KEY ("address_id", "type_id")
 );
 
 CREATE TABLE "persons_documents" (
