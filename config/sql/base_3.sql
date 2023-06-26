49a2152b73aee771a34f401a52b53cd3499c2e5a
Carlos Developer <duard.developer@gmail.com>
2023-06-26 19:24:55 -0300

    feat: fill tables

--- config/sql/base_3.sql
+++ config/sql/base_3.sql
@@ -1,17 +1,26 @@
 CREATE DATABASE persons;
 
 \c persons;
-
 CREATE TABLE "persons" (
   "id" int PRIMARY KEY,
-  "description" varchar(2) NOT NULL
+  "name" varchar(50) NOT NULL
+);
+
+CREATE TABLE "addresses" (
+  "id" int PRIMARY KEY,
+  "person_id" int,
+  "address_line" varchar(100) NOT NULL,
+  "city" varchar(50) NOT NULL,
+  "state" varchar(50) NOT NULL,
+  "country" varchar(50) NOT NULL,
+  FOREIGN KEY ("person_id") REFERENCES "persons" ("id")
 );
 
 CREATE TABLE "persons_documents" (
-  "document_id" char(2) NOT NULL,
-  "person_id" int NOT NULL,
+  "id" int PRIMARY KEY,
+  "person_id" int,
+  "document_id" int,
   "value" varchar(20) NOT NULL,
-  PRIMARY KEY ("document_id"),
   FOREIGN KEY ("person_id") REFERENCES "persons" ("id"),
   FOREIGN KEY ("document_id") REFERENCES "documents" ("id")
 );
@@ -27,4 +36,17 @@ CREATE TABLE "documents" (
   "value" varchar(50) NOT NULL,
   "document_type_id" int,
   FOREIGN KEY ("document_type_id") REFERENCES "document_types" ("id")
+);
+
+CREATE TABLE "persons_types" (
+  "id" int PRIMARY KEY,
+  "type" varchar(50) NOT NULL
+);
+
+CREATE TABLE "persons_type_association" (
+  "person_id" int,
+  "type_id" int,
+  FOREIGN KEY ("person_id") REFERENCES "persons" ("id"),
+  FOREIGN KEY ("type_id") REFERENCES "persons_types" ("id"),
+  PRIMARY KEY ("person_id", "type_id")
 );
